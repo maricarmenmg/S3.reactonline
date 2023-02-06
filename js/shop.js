aa33476447b76d8bf2f0ff73e9a6d5e562ba1148
maricarmen.martin <maricarmenmartin@iMac-de-Maria.local>
2023-02-06 18:22:02 +0100

    Nivel I -  Ejercicio 4

--- js/shop.js
+++ js/shop.js
@@ -1,68 +1,5 @@
-// If you have time, you can move this variable "products" to a json or js file and load the data in this js. It will look more professional
-const products = [
-   {
-        id: 1,
-        name: 'cooking oil',
-        price: 10.5,
-        type: 'grocery',
-        offer: {
-            number: 3,
-            percent: 20
-        }
-    },
-    {
-        id: 2,
-        name: 'Pasta',
-        price: 6.25,
-        type: 'grocery'
-    },
-    {
-        id: 3,
-        name: 'Instant cupcake mixture',
-        price: 5,
-        type: 'grocery',
-        offer: {
-            number: 10,
-            percent: 30
-        }
-    },
-    {
-        id: 4,
-        name: 'All-in-one',
-        price: 260,
-        type: 'beauty'
-    },
-    {
-        id: 5,
-        name: 'Zero Make-up Kit',
-        price: 20.5,
-        type: 'beauty'
-    },
-    {
-        id: 6,
-        name: 'Lip Tints',
-        price: 12.75,
-        type: 'beauty'
-    },
-    {
-        id: 7,
-        name: 'Lawn Dress',
-        price: 15,
-        type: 'clothes'
-    },
-    {
-        id: 8,
-        name: 'Lawn-Chiffon Combo',
-        price: 19.99,
-        type: 'clothes'
-    },
-    {
-        id: 9,
-        name: 'Toddler Frock',
-        price: 9.99,
-        type: 'clothes'
-    }
-]
+
+
 // Array with products (objects) added directly with push(). Products in this array are repeated.
 
 let cartList = [];
@@ -70,7 +7,6 @@ let cartList = [];
 // Improved version of cartList. Cart is an array of products (objects), but each one has a quantity field to define its quantity, so these products are not repeated.
 
 let cart = [];
-
 let total = 0;
 
 
@@ -88,7 +24,7 @@ function buy(id) {
         }
      }
      calculateTotal() 
-     console.log("Add Products Cart", cartList);
+     //console.log("Add Products Cart", cartList);
 }
 
 // Exercise 2
@@ -113,9 +49,31 @@ function calculateTotal() {
 
 // Exercise 4
 function generateCart() {
+
     // Using the "cartlist" array that contains all the items in the shopping cart, 
     // generate the "cart" array that does not contain repeated items, instead each item of this array "cart" shows the quantity of product.
+   //  ℹ️ findIndex: Devuelve el índice del primer elemento de la matriz donde el predicado es verdadero y -1 en caso contrario.
+
+
+let cart = []; // Array cart
+
+ for(let i = 0; i < cartList.length; i++){  // Iteramos en el array cartList.
+    let found = cart.findIndex(producto => producto.id == cartList[i].id); // Buscamos el producto en el array cart.
+       // Si el producto no está en el array cart, lo agregamos. Si no existe, añadimos un nuevo producto al array cart / propiedad "quantity".
+    if(found == -1){
+        cart.push({...cartList[i], quantity: 1, subtotal: cartList[i].price});
+    } else {
+        cart[found].quantity++; // Si ya existe aumenta la cantidad del producto.
+        cart[found].subtotal += cartList[i].price; // Agrega el precio al subtoatal.
+    }
+
 }
+       console.log("Generate Cart", cart);
+}
+
+
+
+
 
 // Exercise 5
 function applyPromotionsCart() {
@@ -145,8 +103,6 @@ function addToCart(id) {
 function removeFromCart(id) {
     // 1. Loop for to the array products to get the item to add to cart
     // 2. Add found product to the cartList array
-
-
 
 
 }
